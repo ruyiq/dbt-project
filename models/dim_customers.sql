23e2da668334ce8ebe09073a7fac758dffa258bd
cassielchiu@outlook.com <cassielchiu@outlook.com>
2023-04-25 03:53:03 +0000

    add staging

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -5,24 +5,12 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
+    SELECT * FROM {{ ref ('stg_customers')}}
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
+    SELECT * FROM {{ ref ('stg_orders')}}
 
 ),
 
